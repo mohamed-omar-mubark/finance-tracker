a09bc5f92e1a6a20bae01d485e990d8150d1182a
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-26 01:16:39 +0200

    feat: :sparkles: summary section in home page with period selection

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Main Page</div>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { transactionViewOptions } from "~/constants";
+const selectedView = ref(transactionViewOptions[1]);
+</script>
