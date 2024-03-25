d94bfb9c40d74d1029bdb2ca769806444c837c3f
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-26 01:10:42 +0200

    feat: :sparkles: add home page and modify user image

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,7 +3,7 @@
     <NuxtLink to="/" class="text-xl font-bold">Finance Tracker</NuxtLink>
     <div>
       <UAvatar
-        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        src="https://avatars.githubusercontent.com/u/88965473?v=4"
         alt="Avatar"
       />
     </div>
