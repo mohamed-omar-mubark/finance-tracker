aa5a1891c0d8569ad0db742c9edf8c14826962c7
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-26 18:03:40 +0200

    feat: :sparkles: deleting transactions and add toast component

--- components/Transaction.vue
+++ components/Transaction.vue
@@ -23,6 +23,7 @@
             color="white"
             variant="ghost"
             trailing-icon="i-heroicons-ellipsis-horizontal"
+            :loading="isLoading"
           />
         </UDropdown>
       </div>
@@ -44,6 +45,30 @@ const iconColor = computed(() =>
 );
 
 const { currency } = useCurrency(props.transaction.amount);
+
+const isLoading = ref(false);
+const toast = useToast();
+
+const supabase = useSupabaseClient();
+const deleteTransaction = async () => {
+  isLoading.value = true;
+  try {
+    await supabase.from("transactions").delete().eq("id", props.transaction.id);
+    toast.add({
+      title: "Transaction deleted",
+      icon: "i-heroicons-check-circle",
+    });
+    emit("deleted", props.transaction.id);
+  } catch (error) {
+    toast.add({
+      title: "Transaction deleted",
+      icon: "i-heroicons-exclamation-circle",
+    });
+  } finally {
+    isLoading.value = false;
+  }
+};
+
 const items = [
   [
     {
@@ -54,7 +79,7 @@ const items = [
     {
       label: "Delete",
       icon: "i-heroicons-trash-20-solid",
-      click: () => console.log("Delete"),
+      click: deleteTransaction,
     },
   ],
 ];
