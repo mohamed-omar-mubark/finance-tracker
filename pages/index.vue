a5674e5347f126564c05d0d08fa54b3050c0eea3
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-26 23:30:49 +0200

    feat: :sparkles: Sorting transactions 2 ways - on backend and on frontend

--- pages/index.vue
+++ pages/index.vue
@@ -107,7 +107,10 @@ const fetchTransactions = async () => {
   isLoading.value = true;
   try {
     const { data } = await useAsyncData("transactions", async () => {
-      const { data, error } = await supabase.from("transactions").select();
+      const { data, error } = await supabase
+        .from("transactions")
+        .select()
+        .order("created_at", { ascending: false });
       if (error) return [];
       return data;
     });
@@ -129,6 +132,14 @@ const transactionsGroupedByDate = computed(() => {
     }
     grouped[date].push(transaction);
   }
+
+  // const sortedKeys = Object.keys(grouped).sort().reverse()
+  // const sortedGrouped = {}
+  // for (const key of sortedKeys) {
+  //   sortedGrouped[key] = grouped[key]
+  // }
+  // return sortedGrouped
+
   return grouped;
 });
 console.log(transactionsGroupedByDate.value);
