d1c59a67574c90ba7a7f2ca6760048c796e51370
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-26 21:09:26 +0200

    feat: :sparkles: Modal Component

--- pages/index.vue
+++ pages/index.vue
@@ -48,11 +48,19 @@
       </div>
     </div>
     <div>
+      <UModal v-model="isOpen">
+        <UCard>
+          <template #header> Add Transaction </template>
+
+          <div>Hello!</div>
+        </UCard>
+      </UModal>
       <UButton
         icon="i-heroicons-plus-circle"
         color="white"
         variant="solid"
         label="Add"
+        @click="isOpen = true"
       />
     </div>
   </section>
@@ -84,6 +92,7 @@ const supabase = useSupabaseClient();
 const selectedView = ref(transactionViewOptions[1]);
 const transactions = ref([]);
 const isLoading = ref(false);
+const isOpen = ref(false);
 
 const income = computed(() =>
   transactions.value.filter((t) => t.type === "Income")
