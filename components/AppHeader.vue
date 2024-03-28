7dd750a1916029bd6be36542f9ff7461845a7661
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-28 15:50:46 +0200

    feat(Auth): :sparkles: Sign-In logic and email magic link

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -9,3 +9,16 @@
     </div>
   </header>
 </template>
+
+<script setup>
+const user = useSupabaseUser();
+watch(
+  user,
+  (user) => {
+    if (user) {
+      console.log(user);
+    }
+  },
+  { immediate: true }
+);
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
   <UCard v-if="!success">
     <template #header>Sign-in to Finance Tracker</template>
 
-    <form>
+    <form @submit.prevent="handleLogin">
       <UFormGroup
         label="Email"
         name="email"
@@ -10,14 +10,15 @@
         :required="true"
         help="You will receive an email with the confirmation link"
       >
-        <UInput type="email" placeholder="Email" required />
+        <UInput v-model="email" type="email" placeholder="Email" required />
       </UFormGroup>
 
       <UButton
         type="submit"
         variant="solid"
         color="black"
-        @click="success = true"
+        :loading="pending"
+        :disabled="pending"
       >
         Sign-in
       </UButton>
@@ -28,7 +29,7 @@
 
     <div class="text-center">
       <p class="mb-4">
-        We have sent an email to <strong>rmsil@email.com</strong> with a link to
+        We have sent an email to <strong>{{ email }}</strong> with a link to
         sign-in.
       </p>
       <p><strong>Important:</strong> The link will expire in 5 minutes.</p>
@@ -38,4 +39,31 @@
 
 <script setup>
 const success = ref(false);
+const email = ref("");
+const pending = ref(false);
+const toast = useToast();
+const supabase = useSupabaseClient();
+const handleLogin = async () => {
+  pending.value = true;
+  try {
+    const { error } = await supabase.auth.signInWithOtp({
+      email: email.value,
+      options: {
+        emailRedirectTo: "http://localhost:3000",
+      },
+    });
+    if (error) {
+      toast.add({
+        title: "Error authenticating",
+        icon: "i-heroicons-exlamation-circle",
+        description: error.message,
+        color: "red",
+      });
+    } else {
+      success.value = true;
+    }
+  } finally {
+    pending.value = false;
+  }
+};
 </script>
