18a36dd5d2c47df2211f0cb829655d7afe58c4be
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-30 01:33:35 +0200

    build: :rocket: Configuring the Finance Tracker App and Deploy it on Vercel

--- pages/login.vue
+++ pages/login.vue
@@ -43,6 +43,7 @@ const email = ref("");
 const pending = ref(false);
 const { toastError } = useAppToast();
 const supabase = useSupabaseClient();
+const redirectUrl = useRuntimeConfig().public.baseUrl;
 
 useRedirectIfAuthenticated();
 
@@ -52,7 +53,7 @@ const handleLogin = async () => {
     const { error } = await supabase.auth.signInWithOtp({
       email: email.value,
       options: {
-        emailRedirectTo: "http://localhost:3000/confirm",
+        emailRedirectTo: `${redirectUrl}/confirm`,
       },
     });
     if (error) {
